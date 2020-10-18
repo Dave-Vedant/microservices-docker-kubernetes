8d070ef1dc97b29bbef5c58dbc3ac64c6db95b20
dave117(Vedant) <vedantdave77@gmail.com>
2020-10-17 23:49:46 -0400

    generate simpe website with node.js
    Use docker alpine - node.js image to generate redis Image in local docker container.

---
+++ simpleweb/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi how are you doing, buddhu?');
+});
+
+app.listen(8080,() => {
+    console.log('Listening on port 8080')
+});
